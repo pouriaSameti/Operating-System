f9d1328628b02431bdd7b64b20b529ea2724a004
pouria <pouria.sameti80@gmail.com>
2023-01-29 15:25:13 +0330

    Accumulator class -> first implementation.

--- phase 1/source code/registers.py
+++ phase 1/source code/registers.py
@@ -21,3 +21,29 @@ class Temp:
         self.__value[0] = -sys.maxsize
 
 
+class Accumulator:
+
+    def __init__(self):
+        self.__value = [-sys.maxsize]
+
+    def reset(self):
+        self.__value[0] = -sys.maxsize
+
+    def is_empty(self):
+        return -sys.maxsize == self.__value[0]
+
+    def set_to_temp(self, temp: Temp):
+        if temp.is_empty():
+            raise Exception("Temp Register is Null.We can't Write into Accumulator")
+
+        self.__value[0] = temp.get()
+
+    def set(self, value):
+        self.__value[0] = value
+
+    def get(self):
+        if self.is_empty():
+            raise Exception("Accumulator Register is Empty")
+
+        return self.__value[0]
+
