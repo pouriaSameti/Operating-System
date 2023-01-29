6fdfef28f3a98efb77fd63b24a420c5f19366f19
pouria <pouria.sameti80@gmail.com>
2023-01-29 15:59:41 +0330

    Process class -> run() -> case(show_context) was added.

--- phase 1/source code/process.py
+++ phase 1/source code/process.py
@@ -74,3 +74,9 @@ class Process:
             acc.set(acc_value)
             temp.set(temp_value)
             self.__current_line = current_line
+
+        if signal == 'show_context':
+            result = self.get_id() + '\n' + ir.__str__() + '\n\n' + acc.__str__() + '\t' + \
+                     temp.__str__() + '\n' + pc.__str__() + '\t\t\t' + self.__state
+
+            print(result)
