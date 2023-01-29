dee1915412988efa66ada79208dc7540f441a67c
pouria <pouria.sameti80@gmail.com>
2023-01-29 15:24:11 +0330

    Temp class -> get() was added.

--- phase 1/source code/registers.py
+++ phase 1/source code/registers.py
@@ -12,9 +12,12 @@ class Temp:
     def set(self, amount: int):
         self.__value[0] = amount
 
+    def get(self):
+        if self.is_empty():
+            raise Exception("Temp Register is Empty")
+        return self.__value[0]
+
     def reset(self):
         self.__value[0] = -sys.maxsize
-
-
 
 
