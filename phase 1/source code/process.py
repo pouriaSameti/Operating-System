d28b47f6fe0f4f4ff14b024ffe27f3d89a0238d2
pouria <pouria.sameti80@gmail.com>
2023-01-29 15:57:34 +0330

    Process class -> run() -> case(run_process) was defined.

--- phase 1/source code/process.py
+++ phase 1/source code/process.py
@@ -28,3 +28,21 @@ class Process:
             self.__state = Process.__states[0]
             os.add_process(self)
             os.send_to_ram(self)
+
+        if signal == 'run_process' and self.__state != Process.__states[2]:
+            self.__state = Process.__states[1]
+
+            pc.set(self.__current_line + self.__start_line)
+
+            instruction, value = os.ram[f'{self.__process_id} {self.__current_line}'].split()
+            ir.set(instruction, int(value))
+
+            acc.set(self.__context['acc'])
+
+            if os.type_instruction(instruction) == 'store':
+                os.store_operate(int(value), temp, acc)
+
+            if os.type_instruction(instruction) == 'arithmetic':
+                os.arithmetic_operate(int(value), instruction, temp, acc)
+
+            self.__current_line += 1
