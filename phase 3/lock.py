0384eb63cd04bad24f489856f572b1f7beecade0
pouria <pouria.sameti80@gmail.com>
2023-01-30 15:22:24 +0330

    Lock class -> wait_producer_right() was added.it designed base on Peterson's algorithm.

--- phase 3/lock.py
+++ phase 3/lock.py
@@ -10,3 +10,9 @@ class Lock:
         while right_producer.value == 1 and turn_produce.value == 1:
             pass
 
+    @classmethod
+    def wait_producer_right(cls, left_producer: Value, right_producer: Value, turn_produce: Value):
+        turn_produce.value = 0
+        right_producer.value = 1
+        while left_producer.value == 1 and turn_produce.value == 0:
+            pass
