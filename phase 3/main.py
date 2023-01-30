1da341777d053bbec6ce9491f313a253272409b1
pouria <pouria.sameti80@gmail.com>
2023-01-30 15:32:33 +0330

    main -> main was changed to initiate algorithms.

--- phase 3/main.py
+++ phase 3/main.py
@@ -1,22 +1,34 @@
 from multiprocessing import Process, Queue, Value
-import LeftSide
 import RightSide
+import LeftSide
+from lock import Lock
 
 
 def main():
+
     street = Value('d', 0)
     id = Value('d', 1)
+
+    turn_produce = Value('d', 0)
+    turn_consume = Value('d', 0)
+
+    left_produce = Value('d', 0)
+    right_produce = Value('d', 0)
+
+    left_consume = Value('d', 0)
+    right_consume = Value('d', 0)
+    lock = Lock()
 
     car_list1 = Queue(maxsize=10)
     car_list2 = Queue(maxsize=10)
 
-    prod1 = Process(target=RightSide.producer, args=(car_list1, id))
-    cons1 = Process(target=RightSide.consumer, args=(car_list1, street))
+    prod1 = Process(target=RightSide.producer, args=(car_list1, id, left_produce, right_produce, turn_produce, lock))
+    cons1 = Process(target=RightSide.consumer, args=(car_list1, street, left_consume, right_consume, turn_consume, lock))
     prod1.start()
     cons1.start()
 
-    prod2 = Process(target=LeftSide.producer, args=(car_list2, id))
-    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street))
+    prod2 = Process(target=LeftSide.producer, args=(car_list2, id, left_produce, right_produce, turn_produce, lock))
+    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street, left_consume, right_consume, turn_consume, lock))
     prod2.start()
     cons2.start()
 
